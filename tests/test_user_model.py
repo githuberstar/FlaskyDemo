956d26850dc5dd64981040bc0af60d7f72291438
ZHU JIHUI@company <zhujihui1991@gmail.com>
2017-04-10 14:13:17 +0800

    'modify'

--- tests/test_user_model.py
+++ tests/test_user_model.py
@@ -1,24 +1,16 @@
 #!/bin/env python
 # -*- coding:utf-8 -*-
-import os
-
+import os, sys, unittest
 import sys
-
-# 加这一段就好了
+import app.auth.views
 from app import create_app
+from app.models import User, Permission, AnonymousUser
 
-#修改系统路径名称
+#修改系统路径名称, chdir改变当前工作目录到指定路径，getcwd返回当前进程的工作目录
 file_folder = os.path.dirname(os.path.abspath(__file__))
 os.chdir(os.path.dirname(file_folder))
 sys.path.append(os.getcwd())
 # sys.path.append(os.path.join(os.path.dirname(os.path.dirname(file_folder)),"app"))
-
-import app.auth.views
-import unittest
-from app.models import User, Permission, AnonymousUser
-
-if __name__ == '__main__':
-    unittest.main()
 
 
 class UserModuleTest(unittest.TestCase):
@@ -26,37 +18,23 @@ class UserModuleTest(unittest.TestCase):
         create_app('default').run()
 
     # def test_password_setter(self):
-      # u = User(password = 'cat')
-      # self.assertTrue(u.password_hash is not None)
-      #
-      # def test_no_password_getter(self):
-      # u = User(password = 'cat')
-      # with self.assertRaises(ArithmeticError):
-      # u.password
-      #
-      # def test_password_verification(self):
-      # u = User(password = 'cat')
-      # self.assertTrue(u.verify_password('cat'))
-      # self.assertFalse(u.verify_password('dog'))
-      #
-      # def test_password_salts_are_random(self):
-      # u = User(password = 'cat')
-      # u2 = User(password = 'cat')
-      # self.assertNotEqual(u.password_hash, u2.password_hash)
-
-    # if __name__ == '__main__':
-    #     -  # app = Flask(__name__)
-    #     -  # ####SMTP server config
-    #     -  # app.config['MAIL_SERVER'] = 'smtp.163.com'  # 电子邮件服务器的地址
-    #     -  # app.config['MAIL_PORT'] = 25  # 邮箱服务器的端口
-    #     -  # app.config['MAIL_USE_TLS'] = True  # 启用安全传输
-    #     -  # app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')  # 邮件账户用户名,已定义环境变量
-    #     -  # app.config['MAIL_PASSWORD'] = os.environ.get(('MAIL_PASSWORD'))  # 邮件账密码,已定义环境变量
-    #     -  # app.config['FLASKY_ADMIN'] = 'aa'
-    #     -  # app.run()
-    #     -    unittest.main()
-
-
+    #     u = User(password = 'cat')
+    #     self.assertTrue(u.password_hash is not None)
+    #
+    # def test_no_password_getter(self):
+    #     u = User(password = 'cat')
+    #     with self.assertRaises(ArithmeticError):
+    #         u.password
+    #
+    # def test_password_verification(self):
+    #     u = User(password = 'cat')
+    #     self.assertTrue(u.verify_password('cat'))
+    #     self.assertFalse(u.verify_password('dog'))
+    #
+    # def test_password_salts_are_random(self):
+    #     u = User(password = 'cat')
+    #     u2 = User(password = 'cat')
+    #     self.assertNotEqual(u.password_hash, u2.password_hash)
 
     def test_roles_and_permissions(self):
         # Role.insert_roles()
@@ -67,3 +45,7 @@ class UserModuleTest(unittest.TestCase):
     def test_anonymous_user(self):
         u = AnonymousUser()
         self.assertFalse(u.can(Permission.FOLLOW))
+
+
+if __name__ == '__main__':
+    unittest.main()
