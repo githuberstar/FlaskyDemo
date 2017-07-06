05a99e88c17e9fa41b417f65e770654716ee4077
Jihui Zhu <zhujihui@gmail.com>
2017-07-06 21:44:28 +0800

    asas

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ class DevelopmentComfig(Config):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     MAIL_HAHAHA = os.environ.get('HAHAHA')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     print 'fuck you !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
     print MAIL_HAHAHA
     print SQLALCHEMY_DATABASE_URI
