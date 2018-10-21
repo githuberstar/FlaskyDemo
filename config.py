17a53fa27b37e5e00cf2440774863848903bd06b
Jihui Zhu <zhujihui@gmail.com>
2018-10-21 20:26:09 +0800

    a

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:zhujihui6578602@207.246.77.81:3306/test?characterEncoding=utf-8'
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:zhujihui6578602@207.246.77.81:3306/test'
 
 
 class ProductionConfig(Config):
